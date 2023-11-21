16a773ee8a14082d5fbd396fd737f804bdf2c118
AnthonyHaro <anthony.haro@epn.edu.ec>
2023-11-21 09:35:01 -0500

    cambio anthony haro

--- carros.java
+++ carros.java
@@ -1,4 +1,5 @@
-
+import java.sql.SQLOutput;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // Press Opt+Enter with your caret at the highlighted text to see how
@@ -9,7 +10,11 @@ public class Main {
         Autos auto2=new Autos("Automovil","Chevrolet","Blanco",2019);
         auto1.imprimirDatos();
         auto2.imprimirDatos();
-
-
+        auto1.setAnio(2000);
+        auto2.setAnio(2005);
+        auto1.setMarca("Nissan");
+        System.out.println("Valores nuevo");
+        auto1.imprimirDatos();
+        auto2.imprimirDatos();
     }
 }
